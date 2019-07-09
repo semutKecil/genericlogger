15d8546f844c48835b2e8a7417afc39c90fa9a11
herlanS_ <n0n1sakuhana>
2019-07-09 12:20:13 +0700

    add to package

--- src/main/java/com/kawanansemut/genericlogger/App.java
+++ src/main/java/com/kawanansemut/genericlogger/App.java
@@ -1,3 +1,5 @@
+package com.kawanansemut.genericlogger;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("hello");
--- src/main/java/com/kawanansemut/genericlogger/SimpleLogger.java
+++ src/main/java/com/kawanansemut/genericlogger/SimpleLogger.java
@@ -1,3 +1,5 @@
+package com.kawanansemut.genericlogger;
+
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
